e5b5c52ea30ec06623786914cf34f517e070f76a
Gaetz <gaetz@rufflerim.com>
2018-02-02 23:35:43 +0100

    Input component for email

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import { Header } from './components/common'
+import { View } from 'react-native';
+import firebase from 'firebase';
+
+import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+    componentWillMount() {
+        const config = {
+            apiKey: 'AIzaSyC0fqjDZ5svMpxt2t1wFfZoBmquJ_eAeck',
+            authDomain: 'react-auth-58991.firebaseapp.com',
+            databaseURL: 'https://react-auth-58991.firebaseio.com',
+            projectId: 'react-auth-58991',
+            storageBucket: 'react-auth-58991.appspot.com',
+            messagingSenderId: '725846966804'
+        };
+        firebase.initializeApp(config);
+    }
+
     render() {
-        return(
+        return (
             <View>
                 <Header title="Authentification" />
-                <Text>An App</Text>
+                <LoginForm />
             </View>
         );
     }
---
+++ src/components/common/Input.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Text, View, TextInput } from 'react-native';
+
+const Input = ({ label, value, placeholder, onChangeText }) => {
+    const { labelStyle, inputStyle, viewStyle } = styles;
+
+    return (
+        <View style={viewStyle}>
+            <Text style={labelStyle}>{label}</Text>
+            <TextInput value={value} onChangeText={onChangeText} style={inputStyle} autoCorrect={false} placeholder={placeholder} />
+        </View>
+    );
+};
+
+const styles = {
+    labelStyle: {
+        fontSize: 18,
+        paddingLeft: 20,
+        flex: 1
+    },
+    inputStyle: {
+        color: '#000',
+        fontSize: 18,
+        paddingRight: 5,
+        paddingLeft: 5,
+        lineHeight: 23,
+        flex: 2
+    },
+    viewStyle: {
+        height: 40,
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center'
+    }
+};
+
+export { Input };
