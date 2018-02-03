2782358c2a0865be8c9445bd5d8b74743a032ce6
Gaetz <gaetz@rufflerim.com>
2018-02-03 09:11:48 +0100

    Auth feedback

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,19 +1,37 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
 import firebase from 'firebase';
-import { Button, Card, CardSection, Input } from './common';
+import { Button, Card, CardSection, Input, Spinner } from './common';
 
 class LoginForm extends Component {
-    state = { email: '', password: '', error: '' };
+    state = { email: '', password: '', error: '', loading: false };
 
     onButtonPress() {
         const { email, password } = this.state;
+        this.setState({error: '', loading: true });
 
-        firebase.auth().signInWithEmailAndPassword(email, password).catch(() => {
-            firebase.auth().createUserWithEmailAndPassword(email, password).catch(() => {
-                this.setState({error: 'Authentification failed'});
-            });
+        firebase.auth().signInWithEmailAndPassword(email, password)
+        .then(this.onLoginSuccess.bind(this))
+        .catch(() => {
+            firebase.auth().createUserWithEmailAndPassword(email, password)
+            .then(this.onLoginSuccess.bind(this))
+            .catch(this.onLoginFailed.bind(this));
         });
+    }
+
+    onLoginSuccess() {
+        this.setState({ email:'', password:'', loading: false, error: '' })
+    }
+
+    onLoginFailed() {
+        this.setState({ error: 'Authentification failed', loading: false })
+    }
+
+    renderButton() {
+        if(this.state.loading) {
+            return <Spinner size='small' />;
+        }
+        return <Button onPress={this.onButtonPress.bind(this)}>Log In</Button>;
     }
 
     render() {
@@ -30,7 +48,7 @@ class LoginForm extends Component {
                 <Text style={styles.errorTextStyle}>{this.state.error}</Text>
 
                 <CardSection>
-                    <Button onPress={this.onButtonPress.bind(this)}>Log In</Button>
+                    {this.renderButton()}
                 </CardSection>
             </Card>
         );
